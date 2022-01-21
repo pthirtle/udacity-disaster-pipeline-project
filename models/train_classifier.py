04de89cdd9a5b41877d4d38f1680203bf2379b30
Paul T <paul.thirtle@hotmail.com>
2022-01-21 17:56:28 +0000

    added build_model

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -14,7 +14,7 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, AdaBoostClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import classification_report
-
+from sklearn.base import BaseEstimator, TransformerMixin
 
 
 def load_data(database_filepath):
@@ -45,11 +45,100 @@ def load_data(database_filepath):
     return X, y, category_names
 
 def tokenize(text):
-    pass
+    """
+    Tokenize Function
+
+    Parameters
+    ----------
+    text : string -> test string to tokenize
+    
+    Returns
+    -------
+    clean_tokens : list -> tokens
+
+    """
+    # replace urls with placeholder
+    # regex to select urls 
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    # get list of all urls using regex
+    detected_urls = re.findall(url_regex, text)
+    # replace each url in text string with placeholder
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    # tokenize text
+    tokens = word_tokenize(text)    
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    # iterate through each token
+    clean_tokens = []
+    for tok in tokens:
+        # lemmatize, normalize case, and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    """
+    Starting Verb Extractor Class
+    
+    This class extracts the starting verb of a sentence, creating a new feature for the ML classifier
+    """
+    
+    def starting_verb(self, text):
+        """
+        
+        """
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+
 
 
 def build_model():
-    pass
+    """
+    Build Model Function
+    
+    Builds a model pipeline including GridSearch
+
+    Returns
+    -------
+    cv_model : model -> the model 
+
+    """
+    # create the pipeline
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+            ('text_pipeline', Pipeline([
+                ('count_vectorizer', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf_transformer', TfidfTransformer())
+            ])),
+            ('starting_verb_transformer', StartingVerbExtractor())
+        ])),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))    
+    ])
+    
+    # set the parameters for grid search
+    parameters = [{ 'clf__estimator__n_estimators': [5, 10],
+                   'clf__estimator__max_depth': [5, 10, None],
+                   'clf__estimator__max_leaf_nodes': [5, 10, None]}]
+    cv_model =  GridSearchCV(pipeline, param_grid=parameters, scoring = 'f1', verbose = 1)
+    
+    return cv_model
+    
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
